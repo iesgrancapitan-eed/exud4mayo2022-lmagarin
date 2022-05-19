43a69e55bfb7c7969d5bd92a3901eb2558aefb0b
lmagarin <lmagarin@users.noreply.github.com>
2022-05-19 14:13:47 +0200

    refactor: nombre del paquete fix #1

---
+++ src/com/mlmc/eed/examen/OptimizacionYDocumentacion/EdadErroneaException.java
@@ -0,0 +1,8 @@
+package com.mlmc.eed.examen.OptimizacionYDocumentacion;
+
+public class EdadErroneaException extends Exception {
+
+	public EdadErroneaException(String string) {
+		super(string);
+	}
+}
