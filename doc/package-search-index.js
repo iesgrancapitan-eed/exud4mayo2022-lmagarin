227115bd95f3fdc88bc068c8c8dec04bb69ee5da
lmagarin <lmagarin@users.noreply.github.com>
2022-05-19 14:58:50 +0200

    docs: documentación javadoc

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.mlmc.eed.examen.OptimizacionYDocumentacion"}];updateSearchResults();
---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.mlmc.eed.examen.OptimizacionYDocumentacion","l":"EdadErroneaException"},{"p":"com.mlmc.eed.examen.OptimizacionYDocumentacion","l":"Mascota"},{"p":"com.mlmc.eed.examen.OptimizacionYDocumentacion","l":"Perro"}];updateSearchResults();
