d72630e42db50b05ee3e623c4067abdcb7e788cf
lmagarin <lmagarin@users.noreply.github.com>
2022-05-19 19:34:45 +0200

    deleted

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.mlmc.eed.examen.OptimizacionYDocumentacion"}];updateSearchResults();
--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.mlmc.eed.examen.OptimizacionYDocumentacion","l":"EdadErroneaException"},{"p":"com.mlmc.eed.examen.OptimizacionYDocumentacion","l":"Mascota"},{"p":"com.mlmc.eed.examen.OptimizacionYDocumentacion","l":"Perro"}];updateSearchResults();
